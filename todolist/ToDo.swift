759cc3d8efe634ef3af353a2de272d00a63777bc
Grace Wentzel <gracefulheart11@gmail.com>
2020-07-14 00:03:59 -0700

    complete iteration 0

---
+++ todolist/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  todolist
+//
+//  Created by Grace Wentzel on 7/13/20.
+//  Copyright © 2020 Grace Wentzel. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
